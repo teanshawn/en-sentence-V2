a998a25b4badf616496cf7454ac5f7f8a731da27
teanshawn <teanshawn@gmail.com>
2022-02-20 03:07:49 +0800

    bat commit automatically : 2022/02/20, 3:07:48

--- target/classes/en-sentence.cmd
+++ target/classes/en-sentence.cmd
@@ -1,20 +1,20 @@
 @echo off
 
-:: 获取当前脚本的路径
+
 java en.EnTestTxtGenerate
-:: 自动提交
+:: commit automatically
 cd ..
 cd ..
 git add . 
- git commit -m "bat批处理自动推送:%date:~0,10%,%time:~0,8%" 
+ git commit -m "bat commit automatically : %date:~0,10%,%time:~0,8%" 
 ::  git commit -m "%commitMessage%" 
 git push origin master
-@echo 已经完成,
+@echo Git push finished
 
-SET daoTime=60
+SET daoTime=10
 :dao
 set /a daoTime=daoTime-1
 ping -n 2 -w 500 127.1>nul
 cls
-echo 上传Git完成 ,倒计时退出: %daoTime%秒
+echo Git push finished, time to exit: %daoTime% second
 if %daoTime%==0 (exit) else (goto dao)
